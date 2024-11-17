bfbbdc5a604aaa62db86509639cf64ed2057058a
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-11-17 20:00:35 +0100

    VueSchool - Vue.js 3 Components Fundamentals - Lesson 1.5

--- courses/vueschool/vuejs-3-components-fundamentals/app.js
+++ courses/vueschool/vuejs-3-components-fundamentals/app.js
@@ -1,26 +1,34 @@
+
+const PlanComponent = {
+    template: '#plan-template',
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+    }
+}
+
+const PlanPickerComponent = {
+    template: '#plan-picker-template',
+    data() {
+        return {
+            plans: [
+                'The Single',
+                'The Curious',
+                'The Addict',
+            ],
+        };
+    },
+    components: {
+        Plan: PlanComponent
+    },
+}
+
 const app = Vue
     .createApp({
-
-    })
-    .component('plan-picker', {
-        template: '#plan-picker-template',
-        data() {
-            return {
-                plans: [
-                    'The Single',
-                    'The Curious',
-                    'The Addict',
-                ],
-            };
-        }
-    })
-    .component('plan', {
-        template: '#plan-template',
-        props: {
-            name: {
-                type: String,
-                required: true,
-            },
+        components: {
+            PlanPicker: PlanPickerComponent
         }
     })
     .mount('#app')
