1ef64e6e3b601e831b3202c5eab47a4da17fe0e6
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-11-17 18:50:14 +0100

    VueSchool - Vue.js 3 Components Fundamentals - Lesson 1.2

--- courses/vueschool/vuejs-3-components-fundamentals/app.js
+++ courses/vueschool/vuejs-3-components-fundamentals/app.js
@@ -3,7 +3,7 @@ const app = Vue
 
     })
     .component('click-counter', {
-        template: '<button @click="count++">{{count}}</button>',
+        template: '#click-counter-template',
         data() {
             return {
                 count: 0
