627fc1cea2493275fac853eb63ff9a4628ab8608
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-08-12 17:39:04 +0000

    Edube - Python Institute - Python Essentials 2 - 4.5.1.22 The datetime and time modules

---
+++ courses/edube/python-institute/pe-2/4.5.1.22 The datetime and time modules/script.py
@@ -0,0 +1,12 @@
+import datetime
+
+
+dt = datetime.datetime(2020, 11, 14, 14, 53, 00)
+
+print(dt.strftime("%Y/%m/%d %H:%M:%S"))  # 2020/11/04 14:53:00
+print(dt.strftime("%y/%B/%d %H:%M:%S %p"))  # 20/November/04 14:53:00 PM
+print(dt.strftime("%a, %Y %b %d"))  # Wed, 2020 Nov 04
+print(dt.strftime("%A, %Y %B %d"))  # Wednesday, 2020 November 04
+print('Weekday: ', dt.strftime("%w"))  # 3
+print('Day of the year: ', dt.strftime("%j"))  # 309
+print('Week number of the year: ', dt.strftime("%U"))  # 44
