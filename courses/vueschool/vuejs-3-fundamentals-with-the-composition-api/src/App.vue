d4f8359cd2ef4083df89f98756b118a617a6b4bd
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-09-16 13:54:54 +0000

    VueSchool - Vue.js 3 Fundamentals with the Composition API - Lesson 1

--- courses/vueschool/vuejs-3-fundamentals-with-the-composition-api/src/App.vue
+++ courses/vueschool/vuejs-3-fundamentals-with-the-composition-api/src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
 import { ref } from "vue";
+
+// Double
 const msg = ref("Hello World!");
 </script>
 
 <template>
+  <!-- {{  }} - is called the vue templating syntax / double mustache syntax -->
   <h1>{{ msg }}</h1>
+
+  <!-- v-model provides two-way data binding for reactive references, 
+       which means if the content is changed in one input it will be 
+       also changed in the other one and the h1 -->
+  <input v-model="msg" />
   <input v-model="msg" />
 </template>
