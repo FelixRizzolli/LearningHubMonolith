3865db636254d65c22acafd5f692ca1fc3d8c224
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-09-16 14:00:20 +0000

    VueSchool - Vue.js 3 Fundamentals with the Composition API - Lesson 2

--- courses/vueschool/vuejs-3-fundamentals-with-the-composition-api/src/App.vue
+++ courses/vueschool/vuejs-3-fundamentals-with-the-composition-api/src/App.vue
@@ -1,17 +1,9 @@
 <script setup>
 import { ref } from "vue";
 
-// Double
-const msg = ref("Hello World!");
+const header = ref("Shopping List App");
 </script>
 
 <template>
-  <!-- {{  }} - is called the vue templating syntax / double mustache syntax -->
-  <h1>{{ msg }}</h1>
-
-  <!-- v-model provides two-way data binding for reactive references, 
-       which means if the content is changed in one input it will be 
-       also changed in the other one and the h1 -->
-  <input v-model="msg" />
-  <input v-model="msg" />
+  <h1>{{ header }}</h1>
 </template>
---
+++ courses/vueschool/vuejs-3-fundamentals-with-the-composition-api/src/Lesson1-App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+
+const msg = ref("Hello World!");
+</script>
+
+<template>
+  <!-- {{  }} - is called the vue templating syntax / double mustache syntax -->
+  <!-- Only one expression is allowed and we also can't declare new variables -->
+  <h1>{{ msg || "Welcome" }}</h1>
+
+  <!-- v-model provides two-way data binding for reactive references, 
+       which means if the content is changed in one input it will be 
+       also changed in the other one and the h1 -->
+  <input v-model="msg" />
+  <input v-model="msg" />
+</template>
