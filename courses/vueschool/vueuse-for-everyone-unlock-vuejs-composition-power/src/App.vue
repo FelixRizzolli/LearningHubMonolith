c53e174771817fdd383820e11cee62f56527aa14
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-09-18 17:12:30 +0200

    VueSchool - VueUse for Everyone: Unlock Vue.js Composition Power - project setup

---
+++ courses/vueschool/vueuse-for-everyone-unlock-vuejs-composition-power/src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { shallowRef, markRaw, ref } from "vue";
+import AppNavbar from "./components/AppNavbar.vue";
+import AppProject from "./components/AppProject.vue";
+
+const navigation = shallowRef([
+  { name: "Project 1", component: markRaw(AppProject), current: true },
+  { name: "Project 2", component: markRaw(AppProject), current: false },
+  { name: "Project 3", component: markRaw(AppProject), current: false },
+]);
+
+const currentNav = ref(
+  navigation.value.find((item) => item.current)?.component ||
+    navigation.value[0].component
+);
+
+function handleNavSelect(component) {
+  currentNav.value = component;
+  navigation.value.forEach(
+    (item) => (item.current = item.component === component)
+  );
+}
+</script>
+
+<template>
+  <header>
+    <AppNavbar :navigation="navigation" @select="handleNavSelect" />
+  </header>
+  <main>
+    <Transition name="fade" mode="out-in">
+      <component :is="currentNav" />
+    </Transition>
+  </main>
+</template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+</style>
---
+++ courses/vueschool/vueuse-for-everyone-unlock-vuejs-composition-power/src/components/AppProject.vue
@@ -0,0 +1,7 @@
+<template>
+    <div
+        class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8"
+    ></div>
+</template>
+
+<script setup></script>
